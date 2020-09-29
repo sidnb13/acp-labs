cd80ef750bd735b93c7c3b56bacfc778ea0220bb
Sidharth Baskaran <sidnbaskaran@gmail.com>
2020-09-29 14:34:10 -0500

    World Data IO lab

---
+++ world-data-io/cmake-build-debug/CMakeFiles/world_data_io.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/world_data_io.dir/main.cpp.o"
+  "world_data_io"
+  "world_data_io.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/world_data_io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
