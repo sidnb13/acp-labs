a8302e0671a82c3fb98471c16e1f1121392750be
Sidharth Baskaran <sidnbaskaran@gmail.com>
2020-10-01 22:44:45 -0500

    Finished lab

--- world-data-io/main.cpp
+++ world-data-io/main.cpp
@@ -4,19 +4,12 @@
 #include <fstream>
 #include <sstream>
 #include <cstdio>
+#include <cctype>
 
 using namespace std;
 
-string getStringAtLoc(const string& s, int idx) {
-    stringstream ip(s);
-    string res{};
-    for (int i{}; i <= idx; i++)
-        getline(ip, res, ',');
-    return res;
-}
-
 struct tpl {
-    string regionName;
+    string name;
     double age{};
 };
 
@@ -26,6 +19,9 @@ struct compare {
     }
 };
 
+string getStringAtLoc(const string& s, int idx);
+bool isUpperString(const string& s);
+
 int main() {
     fstream csv ("WPP2015_MORT_F07_1_LIFE_EXPECTANCY_0_BOTH_SEXES.csv");
     if (!(csv.is_open() && csv.good()))
@@ -34,31 +30,57 @@ int main() {
     string token {};
     for (int i{}; i <= 28; i++) getline(csv, token);
 
-    const vector<string> developedRegions =
-            {"\"NORTHERN AMERICA\"", "\"EUROPE\"","\"Australia/New Zealand\"","\"Japan\"","\"United States of America\""};
+    const vector<string> developedRegions = {"\"NORTHERN AMERICA\"",
+                                             "\"EUROPE\"","\"Australia/New Zealand\"","\"Japan\"",
+                                             "\"United States of America\""};
     // replaces a hash map as would be used in Java
     vector<tpl> developedCountries;
     vector<tpl> allCountries;
+
+    bool isUnderDevelop;
     // get all countries into appropriate vectors
     while (getline(csv, token)) {
-        tpl c;
-        c.regionName = getStringAtLoc(token, 2);
-        c.age = stod(getStringAtLoc(token, 15));
-        allCountries.push_back(c);
-        if (count(developedRegions.begin(), developedRegions.end(), c.regionName))
-            developedCountries.push_back(c);
+        tpl c = {getStringAtLoc(token, 2), stod(getStringAtLoc(token, 15))};
+        bool isRegion = isUpperString(c.name);
+        if (isRegion)
+            isUnderDevelop = count(developedRegions.begin(), developedRegions.end(), c.name);
+        if (!isRegion) {
+            allCountries.push_back(c);
+            if (isUnderDevelop || count(developedRegions.begin(), developedRegions.end(), c.name))
+                developedCountries.push_back(c);
+        }
     }
     csv.close();
+    /*for (const tpl& t : developedCountries)
+        cout << t.name << endl;*/
     // sorting
-    std::sort(developedCountries.begin(), developedCountries.end(), compare());
-    std::sort(allCountries.begin(), allCountries.end(), compare());
+    sort(developedCountries.begin(), developedCountries.end(), compare());
+    sort(allCountries.begin(), allCountries.end(), compare());
 
     for (int i = 0; i < developedCountries.size(); i++) {
-        if (developedCountries.at(i).regionName == "\"United States of America\"")
-            printf("The US is rank #%d of all developed regions.\n", i+1);
+        if (developedCountries.at(i).name == "\"United States of America\"")
+            printf("The US is rank #%d of all developed countries.\n", i+1);
     }
     for (int i = 0; i < allCountries.size(); i++) {
-        if (allCountries.at(i).regionName == "\"United States of America\"")
-            printf("The US is rank #%d of all regions.\n", i+1);
+        if (allCountries.at(i).name == "\"United States of America\"")
+            printf("The US is rank #%d of all countries.\n", i+1);
     }
+}
+
+bool isUpperString(const string& s) {
+    bool t;
+    for (const char& c : s) {
+        if (isupper(c) || c == ' ' || c == '"')
+            t = true;
+        else return false;
+    }
+    return t;
+}
+
+string getStringAtLoc(const string& s, int idx) {
+    stringstream ip(s);
+    string res{};
+    for (int i{}; i <= idx; i++)
+        getline(ip, res, ',');
+    return res;
 }
